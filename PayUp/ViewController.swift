264827c4dc39b19506590d4b05344aa97f18ef51
Rychillie <rychillie@rychillie.net>
2025-08-10 18:54:13 -0300

    Initial Commit

---
+++ PayUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayUp
+//
+//  Created by Rychillie Umpierre de Oliveira on 10/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
